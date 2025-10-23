f4f439748aec7d803680a945b9c8966650f13e19
Jeremy Southern <jezzd11@outlook.com>
2025-10-23 18:53:25 +0100

    first two challenges

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,45 @@
+let squares = [];
+let c1 = 128;
+let c2 = 0;
+let c3 = 128;
+function setup()
+{
+    createCanvas(600,600)
+
+}
+function draw()
+{
+    background(c1,c2,c3);
+    for (let i =0; i < squares.length; i++)
+    {
+        if(i % 2 === 0)
+        {
+            fill(0);
+        }
+        else
+        {
+            fill(255);
+        }
+        rect(squares[i].x,squares[i].y,squares[i].h,squares[i].w)
+    }
+}
+function mouseClicked()
+{
+    c1 = (random(255))
+    c2 = (random(255))
+    c3 = (random(255))
+    squares.length = (0);
+    for(let i =0; i < 100; i++)
+    {
+        squares.shift;
+        let rectangle = 
+        {
+           x:random(600),
+           y:random(600),
+           h:random(5,15),            
+           w:random(5,15)
+        }
+        squares.push(rectangle);
+        console.log(squares);
+    }
+}
--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -1,23 +1,31 @@
-let ball = {
+let ball = 
+{
     x: 50,
     y: 50,
     speedX: 3,
-    speedY: 3
+    speedY: 3,
+    diameter: 100
 }
 
-function setup() {
+function setup() 
+{
     createCanvas(600, 400);
 }
 
-function draw() {
+function draw() 
+{
     background(255);
+    checkBoundaries(ball.x,ball.y);
     circle(ball.x, ball.y, 100);
-    if (ball.x < 50 || ball.x > width - 50) {
+    if (ball.x < 50 || ball.x > width - 50) 
+    {
         ball.speedX *= -1;
     }
-    if (ball.y < 50 || ball.y > height - 50) {
+    if (ball.y < 50 || ball.y > height - 50) 
+    {
         ball.speedY *= -1;
     }
     ball.x += ball.speedX;
     ball.y += ball.speedY;
 }
+
